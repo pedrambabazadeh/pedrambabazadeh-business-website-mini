631165862d4f328163df652adfa31009d582c68d
PEDRAM BABAZADEH <68613205+pedrambabazadeh@users.noreply.github.com>
2025-02-03 19:49:01 +0100

    disappear list menu with MUI in small sizes

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react'
 import {default as data} from './NavBarData'
 import NavBarStructure from './NavBarStructure'
+import { Grid2 as Grid, Box, Grid2 } from '@mui/material'
 import './nav-bar.css'
 export default function NavBar() {
   return (
+    <Grid container display={{xs : 'none', md:'block'}} size={{xs : 12}}>
     <NavBarStructure items={data}/>
+    </Grid>
   )
 }
--- src/Sections/Navigation/NavigationStructure.jsx
+++ src/Sections/Navigation/NavigationStructure.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Grid, Box } from '@mui/material';
+import { Grid2 as Grid, Box } from '@mui/material';
 import { Search, Menu } from '@mui/icons-material';
 
 export default function NavigationStructure(props) {
   return (
     <Grid container spacing={4}>
-      <Grid item xs={6}>
+      <Grid item size={{xs : 6}}>
         <Box className='left-nav' p={2}>
           <div>{props.left}</div>
         </Box>
       </Grid>
-      <Grid item xs={6}>
+      <Grid item size={{xs : 6}}>
         <Box p={2}>{props.center}</Box>
       </Grid>
     </Grid>
