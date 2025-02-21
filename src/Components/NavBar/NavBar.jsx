1b0e9d75b0b6ad13766a287261b2aff1176333b0
PEDRAM BABAZADEH <68613205+pedrambabazadeh@users.noreply.github.com>
2025-02-21 13:40:24 +0100

    resize mobile menu

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import NavBarStructure from './NavBarStructure'
 import MobileMenu from './MobileNavBar'
 import { Grid2 as Grid, Box, Grid2 } from '@mui/material'
 import './nav-bar.css'
-import { Menu } from '@mui/icons-material'
+import { Close, Menu } from '@mui/icons-material'
 export default function NavBar() {
   const [isMenuOpen, setMenuOpen] = useState(false);
   return (
@@ -12,8 +12,8 @@ export default function NavBar() {
     <Grid container display={{xs : 'none', md:'block'}} padding = {{xs: '16px'}} size={{xs : 12}}>
       <NavBarStructure items={data}/>
     </Grid>
-    <Box display={{md: 'none'}} onClick={() => setMenuOpen(!isMenuOpen)}>
-      <Menu />
+    <Box className='menu-icons' display={{md: 'none'}} onClick={() => setMenuOpen(!isMenuOpen)}>
+      {!isMenuOpen ? <Menu /> : <Close/>}
     </Box>
     {isMenuOpen && <Grid container display={{ md:'none'}} size={{xs : 8}} justifyContent={{xs:'end'}}>
       <MobileMenu data={data}/>
